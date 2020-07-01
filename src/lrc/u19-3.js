dd79bd4301d5e2a50fd0248923ee59100e011c12
Domines <719480072@qq.com>
2020-07-01 22:11:40 +0800

    first commit

---
+++ src/lrc/u19-3.js
@@ -0,0 +1,39 @@
+const lrcs = {
+	"ti:": "I Do",
+	"ar:": "Lesson 19",
+	"al:": "ACT III",
+	"by:": "",
+	"00:12.56": "Philip ... Ellen.",
+	"00:13.90": "Ah, Judge.",
+	"00:15.48": "How are you?",
+	"00:16.40": "How are you,my dear?",
+	"00:17.33": "Hello.",
+	"00:17.71": "I think it's time for the wedding to begin.",
+	"00:25.32": "All right, ladies and gentlemen.",
+	"00:26.75": "Please take your places.",
+	"00:28.25": "The wedding ceremony is about to begin.",
+	"00:53.35": "OK, Jane. Start the music.",
+	"01:48.59": "Dearly beloved,",
+	"01:50.24": "we are gathered here today",
+	"01:51.63": "to join this man and this woman in holy matrimony.",
+	"01:57.97": "Do either of you have any reason.",
+	"02:05.49": "Is there anyone present who can show",
+	"02:07.18": "any just cause why these two people",
+	"02:09.61": "should not be legally joined in marriage?",
+	"02:15.64": "Then, Harry Bennett, do you take Susan Stewart",
+	"02:19.49": "to be your lawful, wedded wife?",
+	"02:24.06": "I do.",
+	"02:26.37": "And you, Susan Stewart,",
+	"02:28.60": "do you take Harry Bennett",
+	"02:29.83": "to be your lawful, wedded husband?",
+	"02:33.79": "I do.",
+	"02:35.48": "The rings, please.",
+	"02:40.02": "I have them, Harry.",
+	"03:07.46": "By the power vested in me by the laws of the State of New York,",
+	"03:12.00": "I now pronounce you husband and wife.",
+	"03:16.38": "You may kiss the bride now, Harry."
+}
+
+module.exports = {
+    lrcs: lrcs
+}
